8a23c089a8dc181e08043472d3917e370ed609d0
Matheus Rodrigues <matheusrodrigues@poli.ufrj.br>
2022-10-05 16:33:24 -0300

    fix: added a break in the switch case.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 int main () {
 
     bool firstRun = true;
+    std::string option = "";
+
     do {
 
         if (firstRun == true) {
-            std::cout << "Bem-vindo ao portal das eleições." << std::endl;
+            std::cout << "Bem-vindo ao portal das eleições presidenciais!" << std::endl;
             firstRun = false;
         }
         std::cout << "Por favor, selecione uma das opções abaixo:" << std::endl;
@@ -18,29 +21,33 @@ int main () {
         std::cout << "5 - Qual candidato está na frente" << std::endl;
         std::cout << "0 - Encerrar o programa" << std::endl;
 
-        // TODO use a char to do this, probably getchar
-        std::string insertedOption = "";
-        std::getline (std::cin, insertedOption);
-
-        switch(insertedOption) {
-            case 1:
+        std::getline(std::cin, option);
+            
+        switch(option.at(0)) {
+            case '1':
                 std::cout << "option 1" << std::endl;
-            case 2:
-                std::cout << "option 1" << std::endl;
-            case 3:
-                std::cout << "option 1" << std::endl;
-            case 4:
-                std::cout << "option 1" << std::endl;
-            case 1:
-                std::cout << "option 1" << std::endl;
-            case 1:
-                std::cout << "option 1" << std::endl;
+                break;
+            case '2':
+                std::cout << "option 2" << std::endl;
+                break;
+            case '3':
+                std::cout << "option 3" << std::endl;
+                break;
+            case '4':
+                std::cout << "option 4" << std::endl;
+                break;
+            case '5':
+                std::cout << "option 5" << std::endl;
+                break;
+            case '0':
+                std::cout << "Programa encerrado." << std::endl;
+                exit (EXIT_SUCCESS);
 
             default:
                 std::cout << "Opção inválida, tente novamente." << std::endl;
         }
 
-    } while (insertedOption != 0);
+    } while (option.at(0) != 0);
 
     return 0;
 }
